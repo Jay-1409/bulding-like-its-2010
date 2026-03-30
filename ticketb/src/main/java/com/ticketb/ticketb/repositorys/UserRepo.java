d84c21e41f7703ecf6d3adf0c9d2a98baaab7e99
Jay Shah <jay_shah@outlook.in>
2026-03-30 17:30:36 +0530

    add the userService code & user controller Code

---
+++ ticketb/src/main/java/com/ticketb/ticketb/repositorys/UserRepo.java
@@ -0,0 +1,8 @@
+package com.ticketb.ticketb.repositorys;
+
+import com.ticketb.ticketb.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {}
