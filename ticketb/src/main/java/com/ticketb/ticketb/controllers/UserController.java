6f3df5f20797436278067a5ece593745e19663b8
Jay Shah <jay_shah@outlook.in>
2026-04-01 18:58:18 +0530

    add controllers for login&signup

--- ticketb/src/main/java/com/ticketb/ticketb/controllers/UserController.java
+++ ticketb/src/main/java/com/ticketb/ticketb/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.ticketb.ticketb.controllers;
 
 import com.ticketb.ticketb.entities.User;
 import com.ticketb.ticketb.services.UserService;
+
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +30,26 @@ public class UserController {
             );
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    public ResponseEntity<?> login(@RequestBody User user) {
+        try {
+            return new ResponseEntity<>(
+                userService.Login(user),
+                HttpStatus.OK
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    public ResponseEntity<?> signup(@RequestBody User usr) {
+        try {
+            return new ResponseEntity<>(
+                userService.AddUser(usr),
+                HttpStatus.OK
+            );
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
