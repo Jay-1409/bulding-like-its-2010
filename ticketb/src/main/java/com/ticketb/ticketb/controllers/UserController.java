53f8970f1649f8e790beda27484ea3e251da9923
Jay Shah <jay_shah@outlook.in>
2026-04-01 20:26:57 +0530

    add redis

--- ticketb/src/main/java/com/ticketb/ticketb/controllers/UserController.java
+++ ticketb/src/main/java/com/ticketb/ticketb/controllers/UserController.java
@@ -1,15 +1,14 @@
 package com.ticketb.ticketb.controllers;
 
-import com.ticketb.ticketb.entities.User;
-import com.ticketb.ticketb.services.UserService;
-
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ticketb.ticketb.entities.User;
+import com.ticketb.ticketb.services.UserService;
 
 @RestController
 @RequestMapping("/users")
---
+++ ticketb/src/main/java/com/ticketb/ticketb/services/RedisService.java
@@ -0,0 +1,8 @@
+package com.ticketb.ticketb.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+    
+}
